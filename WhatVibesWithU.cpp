ebf9ce37d682b8cd7e2dd8fe3a4fc81a948d3f12
Invilis <wonderwaffle07@proton.me>
2025-03-24 03:07:48 -0600

    Added basic structures for app state (ComparableItem & Category), also a basic menu bar (does nothing rn).

--- WhatVibesWithU.cpp
+++ WhatVibesWithU.cpp
@@ -17,6 +17,9 @@
 #include <GLFW/glfw3.h> // Includes OpenGL headers
 
 #include <fstream>
+#include <functional>
+#include <limits>
+#include <string>
 
 struct Color {
 	constexpr Color(uint8_t a, uint8_t r, uint8_t g, uint8_t b) : a(a), r(r), g(g), b(b) {}
@@ -56,13 +59,53 @@ namespace Colors {
 	constexpr Color VividSkyBlue{ 0xFF00CCFF };
 }
 
-struct AppState {
+namespace AppState {
 	enum Page : uint8_t {
 		TITLE,
 		DECISION_MATRIX,
 		TOURNAMENT_BRACKET
-	} page{ DECISION_MATRIX };
-};
+	} page{ TITLE };
+
+	struct Category {
+		inline double calcFinalScore(double x) {
+			finalScore = weight * scoringFunction(x);
+			return finalScore;
+		}
+
+		Category(std::string _name = "Unnamed", double init_weight = 1.0, std::function<double(double)> scoreFunc = [](double x) { return x; }) : name(_name), weight(init_weight), scoringFunction(scoreFunc) {}
+
+		std::string name;
+		double weight{ 1.0 };
+		double finalScore{ 0.0 };
+		double maxScore{ std::numeric_limits<double>::infinity() };
+		double minScore{ -std::numeric_limits<double>::infinity() };
+		std::function<double(double)> scoringFunction{ [](double x) { return x; } };
+	};
+
+	struct ComparableItem {
+		inline void calcFinalScore() {
+			finalScore = 0.0;
+			for (size_t i = 0; i < categories.size(); i++) {
+				finalScore += categories[i].calcFinalScore(categoryScores[i]);
+			}
+		}
+
+		ComparableItem() : ComparableItem("Unnamed") {}
+		ComparableItem(std::string _name) : name(_name) { categoryScores.resize(categories.size()); }
+
+		std::string name;
+		std::vector<double> categoryScores; // One score per Category
+		double finalScore{ 0.0 };
+	};
+
+	inline void pushCategory(const Category& category) {
+		categories.push_back(category);
+		for (ComparableItem& thing : things) { thing.categoryScores.push_back(0.0); }
+	}
+
+	std::vector<Category> categories;
+	std::vector<ComparableItem> things;
+}
 
 static std::ofstream logFile("log.txt");
 
@@ -78,6 +121,10 @@ std::string latestSupportedGLSLVersion() {
 	size_t i = versionString.find_first_of('.');
 	size_t j = versionString.find_first_not_of("0123456789", i + 1);
 	return "#version " + versionString.substr(0, i) + versionString.substr(i, j);
+}
+
+inline double linearFunc(double x, double slope = 1.0, double intercept = 0.0) {
+	return slope * x + intercept;
 }
 
 int main() {
@@ -137,6 +184,10 @@ int main() {
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
+	ImGui::StyleColorsDark();
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.FrameRounding = 4.0f; // Windows 11: ControlCornerRadius
+
 	// Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	logFile << "Using GLSL version: " << glsl_version << '\n';
@@ -151,7 +202,11 @@ int main() {
 		logFile << "Failed to load font file: " << FONTS[DEFAULT] << '\n';
 	}
 
-	AppState state;
+	#define TEST
+	#ifdef TEST /// TEST
+	AppState::pushCategory({ "Cost", 2.0, [](double x) { return 10.0 - x/100.0; } });
+	AppState::things.push_back({ "Thing 1" });
+	#endif /// TEST
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -172,16 +227,55 @@ int main() {
 		ImGui::SetNextWindowPos({ vec.x, vec.y });
 		ImGui::Begin("VibeDiff", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
 
-		if (state.page == AppState::TITLE) {
-			state.page =
-				ImGui::Button("Decision Matrix") ? AppState::DECISION_MATRIX
-				: (ImGui::Button("Tournament Bracket") ? AppState::TOURNAMENT_BRACKET
+		if (AppState::page == AppState::TITLE) {
+			/// Title page ///
+			float size = ImGui::CalcTextSize("Tournament Bracket").x + style.FramePadding.x * 2.0f;
+    		float avail = ImGui::GetContentRegionAvail().x;
+			float xOffset = (avail - size) * 0.5f;
+
+			ImGui::SetCursorPosX(ImGui::GetCursorPosX() + xOffset);
+			bool buttonStatus_DM = ImGui::Button("Decision Matrix", { size, 0 });
+			ImGui::SetCursorPosX(ImGui::GetCursorPosX() + xOffset);
+			bool buttonStatus_TB = ImGui::Button("Tournament Bracket", { size, 0 });
+
+			AppState::page =
+				buttonStatus_DM ? AppState::DECISION_MATRIX
+				: (buttonStatus_TB ? AppState::TOURNAMENT_BRACKET
 				: AppState::TITLE);
 		}
-		else {
-			switch (state.page) {
+		else { /// not the title page 😛 ///
+			if (ImGui::BeginMainMenuBar()) {
+				if (ImGui::BeginMenu("File")) {
+					if (ImGui::BeginMenu("Export")) {
+						if (ImGui::MenuItem("Image...")) {}
+						ImGui::EndMenu();
+					}
+					if (ImGui::BeginMenu("Import")) {
+						if (ImGui::MenuItem("From text file...")) {}
+						ImGui::EndMenu();
+					}
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Edit")) {
+					if (ImGui::MenuItem("Undo", "CTRL+Z", false, false)) {}
+					if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}
+					ImGui::Separator();
+					if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+					if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+					if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+					ImGui::Separator();
+					if (ImGui::MenuItem("Variables...", "CTRL+SHIFT+V")) {}
+					if (ImGui::MenuItem("Functions...", "CTRL+SHIFT+F")) {}
+					ImGui::EndMenu();
+				}
+				ImGui::EndMainMenuBar();
+			}
+
+			switch (AppState::page) {
 			case AppState::DECISION_MATRIX:
-				/* code */
+				for (size_t i = 0; i < AppState::things.size(); i++) {
+					ImGui::Text(AppState::things[i].name.c_str());
+				}
 				break;
 			case AppState::TOURNAMENT_BRACKET:
 				/* code */
